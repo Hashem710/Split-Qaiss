81b46e13378b0cfab5ef8a969545098cbe1436d2
Hashem710 <154567114+Hashem710@users.noreply.github.com>
2025-05-12 00:28:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrollToPlans() {
+    document.getElementById('plans').scrollIntoView({ behavior: 'smooth' });
+  }
+function scrollToContact() {
+    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+  }
+function scrollToAbout() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+  }  
